4b2e567961f647676d0374fa6edddc2278718d18
Hei-Lima <heitor.lipeixoto@gmail.com>
2025-05-07 21:49:43 -0300

    First

---
+++ js/app.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("form");
+    const url = document.getElementById("url");
+    const expiration = document.getElementById("expirationDate");
+
+
+    function showUrl() {
+        
+    }
+
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+
+        let fetchObject = {
+            url: url.value,
+            expirationDate: expiration.value,
+        }
+
+        alert(JSON.stringify(fetchObject))
+
+        fetch(`http://127.0.0.1:8080/api/shorten`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(fetchObject)
+        }).then(response => response.json())
+        .then(data => showUrl(data))
+        .catch(error => alert(`Erro: ${error.message}`))                
+    }
+    )
+}
+) 
